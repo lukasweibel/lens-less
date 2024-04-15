62b5cbbfbdde8753f9538fd99b92cfa7f28b52d1
Lukas Weibel <lukasweibel@macbook-air-von-lukas.home>
2024-04-15 20:32:39 +0200

    Add GitHub actions

--- api/service.py
+++ api/service.py
@@ -8,3 +8,6 @@ app = Flask(__name__, static_folder='./../frontend/public', static_url_path='')
 @app.route('/')
 def index():
     return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=80)
