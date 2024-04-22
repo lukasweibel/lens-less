3d39854cdde870c2affef4ae9c74ff4a17035a68
Lukas Weibel <lukasweibel@air-von-lukas.zhaw.ch>
2024-04-22 18:16:30 +0200

    Add endpoint and Readme

--- api/service.py
+++ api/service.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='./../frontend/public', static_url_path='')
@@ -6,6 +6,12 @@ app = Flask(__name__, static_folder='./../frontend/public', static_url_path='')
 @app.route('/')
 def index():
     return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/data', methods=['POST'])
+def postData():
+    data = request.json
+    print(data)
+    return {"received_data": data}, 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
