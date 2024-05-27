ae4e8ed6caa86f3ba8b673c9209ddaeefaec90d9
Lukas Weibel <lukasweibel@air-von-lukas.zhaw.ch>
2024-05-27 19:38:56 +0200

    Add mongodb save logic

--- api/service.py
+++ api/service.py
@@ -1,9 +1,10 @@
 import os
+import json
 from flask import Flask, Response, request
 from flask import Flask, send_from_directory
 from backend.blob_accessor import upload_picture, load_picture
 from backend.image_generation import generate_picture
-from backend.mongodb_accessor import add_entry_to_db
+from backend.mongodb_accessor import add_entry_to_db, get_all_entries
 
 app = Flask(__name__, static_folder='./../frontend/public', static_url_path='')
 
@@ -16,10 +17,9 @@ def postData():
     data = request.json
     print(data)
     prompt = create_prompt(data)
-    add_entry_to_db(prompt)
-    print(prompt)
     picture = generate_picture(prompt)
     pictureId = upload_picture(picture)
+    add_entry_to_db(prompt, pictureId, data)
     return {"received_data": data, "pictureId": pictureId}, 200
 
 @app.route('/picture', methods=['POST'])
@@ -33,6 +33,11 @@ def uploadPicture():
 @app.route('/picture/<pictureId>', methods=['GET'])
 def downloadPicture(pictureId):
     return Response(load_picture(pictureId+'.png') , mimetype='image/png')
+
+@app.route('/history', methods=['GET'])
+def getHistory():
+    data = json.dumps(get_all_entries())  # Serialize data to a JSON formatted string
+    return Response(data.encode('utf-8'), mimetype='application/json')
 
 def create_prompt(sensor_data):
     latitude = sensor_data.get("latitude")
--- backend/mongodb_accessor.py
+++ backend/mongodb_accessor.py
@@ -1,6 +1,9 @@
 import os
+import json
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from datetime import datetime
+from bson import ObjectId
 
 load_dotenv()
 
@@ -13,20 +16,27 @@ db = client['mdm']
 collection = db['iot-lens-less']
 
 
-def add_entry_to_db(prompt):
-    document = {
-        'prompt': prompt
-    }
+class CustomJSONEncoder(json.JSONEncoder):
+    """ Custom JSON Encoder that handles non-serializable types. """
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        elif isinstance(obj, ObjectId):  # MongoDB ObjectId is also not serializable by default
+            return str(obj)
+        return super().default(obj)
 
+def add_entry_to_db(prompt, picture_id, original_data):
+    document = {
+        'created': datetime.now(),
+        'prompt': prompt,
+        'pictureId': picture_id,
+        'originalData': original_data
+    }
     collection.insert_one(document)
     print("Entry added to database.")
 
 def get_all_entries():
     documents = collection.find({})
     documents_list = list(documents)
-    cleaned_data = [{k: v for k, v in d.items() if k != '_id'} for d in documents_list]
-    return cleaned_data
-
-
-
-    
+    # Encode directly within the function to ensure all data is properly handled
+    return json.dumps(documents_list, cls=CustomJSONEncoder)
